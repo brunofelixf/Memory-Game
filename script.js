1a9958676e86265ec8a9905292b9f92ada1f5cec
brunofelixf <brunofelixf@gmail.com>
2021-12-05 12:26:54 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cards = document.querySelectorAll('.card')
 let hasFlippedCard = false
 let firstCard, secondCard
+let lockBoard = false
+
 //função para virar carta
 function flipCard() {
+  if (lockBoard) return
   if (this === firstCard) return
 
   this.classList.add('flip')
@@ -29,15 +32,36 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard)
   secondCard.removeEventListener('click', flipCard)
+
+  resetBoard()
 }
 
 //funcão que desvira as cartas
 function unflipCards() {
+  lockBoard = true
+
   setTimeout(() => {
     firstCard.classList.remove('flip')
     secondCard.classList.remove('flip')
+
+    resetBoard()
   }, 1500)
 }
+
+//função que reseta o tabuleiro
+function resetBoard() {
+  ;[hasFlippedCard, lockBoard] = [false, false]
+  ;[firstCard, secondCard] = [null, null]
+}
+
+//função que embaralha as cartas
+;(function shuffle() {
+  cards.forEach(card => {
+    let ramdomPosition = Math.floor(Math.random() * 12)
+    card.style.order = ramdomPosition
+  })
+})()
+
 cards.forEach(card => {
   card.addEventListener('click', flipCard)
 })

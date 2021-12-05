0f24a8e115e77f221b9126912f96e16e81005e1f
brunofelixf <brunofelixf@gmail.com>
2021-12-05 11:41:44 -0300

    Parar de virar cartas

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const cards = document.querySelectorAll('.card')
-
+let hasFlippedCard = false
+let firstCard, secondCard
+//função para virar carta
 function flipCard() {
-  this.classList.toggle('flip')
+  if (this === firstCard) return
+
+  this.classList.add('flip')
+  if (!hasFlippedCard) {
+    hasFlippedCard = true
+    firstCard = this
+    return
+  }
+
+  secondCard = this
+  hasFlippedCard = false
+  checkForMatch()
+}
+//função que checa se as cartas são iguais
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards()
+    return
+  }
+
+  unflipCards()
 }
 
 cards.forEach(card => {
